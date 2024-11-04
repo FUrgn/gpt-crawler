b6062e3c58d52ecfc14d19102827885bc7e4cc85
FUrgn <sonnneeeee@gmail.com>
2024-11-04 22:30:46 +0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -3,7 +3,8 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://www.builder.io/c/docs/developers",
   match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
+  selector: `.docs-builder-container`
+  maxPagesToCrawl: 1000,
   outputFileName: "output.json",
   maxTokens: 2000000,
 };
